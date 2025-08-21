25d228aef5199e401fc37f944eaaa06b64116057
Emanuel <emanuel.carbajalsantacruz@gmail.com>
2025-08-20 20:43:03 -0600

    <Update> components.js fetch url

--- js/components/loadComponent.js
+++ js/components/loadComponent.js
@@ -1,6 +1,6 @@
 export async function loadComponent(id, file){
     try{
-        const response = await fetch(`http://localhost:5500/html/${file}`);
+        const response = await fetch(`https://emanuek899.github.io/ExpandedTaleWebPage/html/${file}`);
         const content = await response.text();
         document.getElementById(id).innerHTML = content;
     }catch(error){
